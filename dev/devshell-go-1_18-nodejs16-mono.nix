afc619015ef718c712aec6543dce76a55df20b40
ppenguin <hieronymusv@gmail.com>
2022-12-13 14:14:56 +0100

    add go-1.18 nodejs-16 devshell

---
+++ dev/devshell-go-1_18-nodejs16-mono.nix
@@ -0,0 +1,18 @@
+
+{ pkgs ? import <nixpkgs> { config.allowUnfree = true; }
+, unstable ? import <unstable> { config.allowUnfree = true; }
+}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    gnumake mono nodejs-16_x yarn
+    sqlite
+    postman
+  ] ++ (with unstable; [
+    go_1_18 gopls
+    # delve !!! probably enforce this: go get github.com/go-delve/delve, which might work to solve this issue: https://github.com/microsoft/vscode-go/issues/1421
+  ]);
+  shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.sqlite.out}/lib
+  '';
+}
