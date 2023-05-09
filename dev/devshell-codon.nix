0150f2f1909274c2b5ceba2fffcb6e4265cd7bbe
ppenguin <hieronymusv@gmail.com>
2023-05-09 10:28:26 +0200

    add codon devshell

---
+++ dev/devshell-codon.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {}
+, unstable ? import <unstable> {}
+}:
+# !!! if using unstable, use it for all pkgs, otherwise library errors during build!
+let
+  inherit (pkgs) lib;
+  myllvm = unstable.llvmPackages_15;
+  py = unstable.python310;
+  pypkgs = [ (py.withPackages (p: with p; [ numpy pint ]))];
+in
+(unstable.mkShell.override { stdenv = myllvm.stdenv; }) {
+  nativeBuildInputs = with unstable; [
+    codon
+  ] ++ pypkgs;
+
+  # shellHook = ''
+  # '';
+}
