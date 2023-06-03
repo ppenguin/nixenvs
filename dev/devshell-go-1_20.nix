e7407cbd31db1eaf077c133b89d41c10a1cf693d
ppenguin <hieronymusv@gmail.com>
2023-06-03 20:32:05 +0200

    add devshel go 1.20

---
+++ dev/devshell-go-1_20.nix
@@ -0,0 +1,13 @@
+
+{ pkgs ? import <nixpkgs> { config.allowUnfree = true; }
+, unstable ? import <unstable> { config.allowUnfree = true; }
+}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    gnumake
+    go_1_20 gopls delve
+  ];
+  # shellHook = ''
+  # '';
+}
